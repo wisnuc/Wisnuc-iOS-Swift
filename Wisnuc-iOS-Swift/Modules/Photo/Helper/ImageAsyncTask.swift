d8b37fbda5402974a94fe5e95dbffc844e53cab6
charlessnow <1126104236@qq.com>
2018-12-17 21:10:01 +0800

    new fix

---
+++ Wisnuc-iOS-Swift/Modules/Photo/Helper/ImageAsyncTask.swift
@@ -0,0 +1,13 @@
+//
+//  ImageAsyncTask.swift
+//  Wisnuc-iOS-Swift
+//
+//  Created by wisnuc-imac on 2018/12/17.
+//  Copyright © 2018 wisnuc-imac. All rights reserved.
+//
+
+import UIKit
+
+class ImageAsyncTask: NSObject {
+
+}
